448962d79101b50d74a24eec9b4e58149ce4fb5f
o.abdulaziz <aoqilov88@gmail.com>
2025-12-20 00:38:41 +0500

    feat: add image display and update styles in ChairsPage

--- src/components/pages/gallery/chairs-page/ChairsPage.tsx
+++ src/components/pages/gallery/chairs-page/ChairsPage.tsx
@@ -3,37 +3,54 @@ import React, { useEffect, useRef, useState } from "react";
 import "./chairspage.scss";
 import BigSlider from "./sections/big-slider/BigSlider";
 import FilterSection from "./sections/filter-section/FilterSection";
+import defimage from "../../../../../public/ess.png";
+import Image from "next/image";
 
 const ChairsPage = () => {
-  const [fixed, setFixed] = useState(false);
   const filterRef = useRef<HTMLDivElement>(null);
-  const [height, setHeight] = useState(0);
-
-  useEffect(() => {
-    if (filterRef.current) {
-      setHeight(filterRef.current.offsetHeight);
-    }
-
-    const onScroll = () => {
-      setFixed(window.scrollY >= 150);
-    };
-
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
 
   return (
     <div className="chairspage">
       <BigSlider />
 
       {/* placeholder */}
-      {fixed && <div style={{ height }} />}
 
-      <div ref={filterRef} className={fixed ? "filterFixed" : "filterNormal"}>
+      <div ref={filterRef}>
         <FilterSection />
       </div>
 
-      <div className="content">grid setka</div>
+      <div className="contentsetka">
+        <div className="image-container">
+          <Image src={defimage} alt="Chair" style={{ objectFit: "cover" }} />
+        </div>
+        <div className="image-container">
+          <Image src={defimage} alt="Chair" style={{ objectFit: "cover" }} />
+        </div>
+        <div className="image-container">
+          <Image src={defimage} alt="Chair" style={{ objectFit: "cover" }} />
+        </div>
+        <div className="image-container">
+          <Image src={defimage} alt="Chair" style={{ objectFit: "cover" }} />
+        </div>
+        <div className="image-container">
+          <Image src={defimage} alt="Chair" style={{ objectFit: "cover" }} />
+        </div>
+        <div className="image-container">
+          <Image src={defimage} alt="Chair" style={{ objectFit: "cover" }} />
+        </div>
+        <div className="image-container">
+          <Image src={defimage} alt="Chair" style={{ objectFit: "cover" }} />
+        </div>
+        <div className="image-container">
+          <Image src={defimage} alt="Chair" style={{ objectFit: "cover" }} />
+        </div>
+        <div className="image-container">
+          <Image src={defimage} alt="Chair" style={{ objectFit: "cover" }} />
+        </div>
+        <div className="image-container">
+          <Image src={defimage} alt="Chair" style={{ objectFit: "cover" }} />
+        </div>
+      </div>
     </div>
   );
 };
